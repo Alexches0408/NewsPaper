0fc0d3613a3f40f8730269f58ccb8f4fd000c504
Alexches0408 <103947115+Alexches0408@users.noreply.github.com>
2023-03-13 11:39:28 +0800

    Add files via upload

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,6 +46,8 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -56,7 +63,7 @@ ROOT_URLCONF = 'NewsPaper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
